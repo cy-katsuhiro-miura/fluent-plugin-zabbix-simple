b7a5a7547219a3d5e68b68e62895eadfab11ab96
katsuhiro-miura <katsuhiro_miura@cybird.co.jp>
2013-08-10 14:27:44 +0900

    outputをBufferedOutputにしてRetry可能に

--- lib/fluent/plugin/out_zabbix_simple.rb
+++ lib/fluent/plugin/out_zabbix_simple.rb
@@ -15,7 +15,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
-class Fluent::ZabbixSimpleOutput < Fluent::Output
+class Fluent::ZabbixSimpleOutput < Fluent::BufferedOutput
   Fluent::Plugin.register_output('zabbix_simple', self)
 
   def initialize
